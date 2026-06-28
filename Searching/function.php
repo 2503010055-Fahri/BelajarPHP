6e1e03ea9678dd2c081e01daa7112a9ef5574ed1
2503010055-Fahri <2503010055@unper.ac.id>
2026-06-28 19:34:49 +0700

    search data PHP dasar

---
+++ Searching/function.php
@@ -0,0 +1,71 @@
+<?php
+// Koneksi database
+$conn = mysqli_connect("localhost", "root", "", "phpdasar");
+
+function query($query)
+{
+    global $conn;
+    $result = mysqli_query($conn, $query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+function tambah($data)
+{
+    global $conn;
+
+    $nama = htmlspecialchars($data["nama"]);
+    $nim = htmlspecialchars($data["nim"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+    $gambar = htmlspecialchars($data["gambar"]);
+
+
+    // query insert data
+    $query = "INSERT INTO mahasiswa VALUES ('', '$nama', '$nim', '$email', '$jurusan', '$gambar')";
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+// hapus
+function hapus($id)
+{
+    global $conn;
+    mysqli_query($conn, "DELETE FROM mahasiswa WHERE id = $id");
+    return mysqli_affected_rows($conn);
+}
+
+
+// update
+function update($data)
+{
+    global $conn;
+
+    $id = $data["id"];
+    $nama = htmlspecialchars($data["nama"]);
+    $nim = htmlspecialchars($data["nim"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+    $gambar = htmlspecialchars($data["gambar"]);
+
+
+    // query insert data
+    $query = "UPDATE mahasiswa SET nama = '$nama', nim = '$nim', email = '$email', jurusan = '$jurusan', gambar = '$gambar'
+              WHERE id = $id";
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+// Fungsi cari berdasarkan nama,nim, atau jurusan
+ function cari($keyword){
+    $query = "SELECT * FROM mahasiswa WHERE nama LIKE '%$keyword%' OR nim LIKE '%$keyword%' OR jurusan LIKE '%$keyword%'";
+    return query($query);
+ }
+
+
+?>
---
+++ Searching/update.php
@@ -0,0 +1,73 @@
+<?php
+require("function.php");
+// konek database
+$conn = mysqli_connect("localhost", "root", "", "phpdasar");
+
+// ambil data dari URL
+$id = $_GET["id"];
+// query data mahasiswa berdasarkan id-nya
+$mhs = query("SELECT * FROM mahasiswa WHERE id = $id")[0];
+
+// cek apakah tobol submit sudah di tekan?
+if( isset($_POST["submit"]) ){
+
+    // cek apakah data berhasil diupdate
+    if (update($_POST) > 0){
+        echo "<script>
+                alert('data berhasil diupdate')
+                document.location.href = 'index.php'
+            </script>";
+    } else {
+        echo "<script>
+                alert('data gagal diupdate')
+                documnet.location.href = 'index.php'
+            </script>";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Data Mahasiswa</title>
+</head>
+<body>
+    
+    <h1>Update Data Mahasiswa</h1>
+
+    <form action="" method="post">
+        <input type="hidden" name="id" value="<?= $mhs["id"] ?>">
+        <ul>
+            <li>
+                <label for="nama">Nama : </label>
+                <input type="text" name="nama" id="nama" required value="<?= $mhs["nama"] ?>">
+            </li>
+            <li>
+                <label for="nim">NIM : </label>
+                <input type="text" name="nim" id="nim" required value="<?= $mhs["nim"] ?>">
+            </li>
+            <li>
+                <label for="email">Email : </label>
+                <input type="email" name="email" id="email" required value="<?= $mhs["email"] ?>">
+            </li>
+            <li>
+                <label for="jurusan">Jurusan : </label>
+                <input type="text" name="jurusan" id="jurusan" required value="<?= $mhs["jurusan"] ?>">
+            </li>
+            <li>
+                <label for="gambar">Gambar : </label>
+                <input type="text" name="gambar" id="gambar" required value="<?= $mhs["gambar"] ?>">
+            </li>
+            <li>
+                <button type="submit" name="submit">Simpan</button>
+            </li>
+            <li>
+                <a href="index.php">Kembali ke daftar mahasiswa</a>
+            </li>
+        </ul>
+    </form>
+
+</body>
+</html>
