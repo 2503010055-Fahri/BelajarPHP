107d3f5f1b9cd92ccbdd2ffcafd45db6608fb9e3
2503010055-Fahri <2503010055@unper.ac.id>
2026-06-29 14:32:59 +0700

    membuat registrasi dan upload di data mahasiswa PHP dasar

---
+++ Upload/index.php
@@ -0,0 +1,70 @@
+<?php 
+require 'function.php';
+// tampilkan seluruh data mahasiswa dimna dari data yang terbaru ke terlama
+$mahasiswa = query("SELECT * FROM mahasiswa");
+
+// tombol cari ditekan
+if( isset($_POST["cari"]) ){
+    $mahasiswa = cari($_POST["keyword"]);
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+<body>
+    
+
+    <h1>Daftar Mahasiswa</h1>
+
+    <!-- tambah data mahasiswa -->
+     <a href="tambah.php">Tambah Data Mahasiswa</a>
+     <br>
+     <br>
+
+     <!-- search -->
+      <form action="" method="post">
+        <input type="text" name="keyword" size="40" autofocus placeholder="masukan keyword penacarian..." autocomplete="off">
+        <button type="submit" name="cari">Cari</button>
+      </form>
+
+      <br>
+
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Gambar</th>
+            <th>NIM</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+        </tr>
+
+        <?php $i = 1 ?>
+        <?php foreach( $mahasiswa as $row ) : ?>
+        <tr>
+            <td><?= $i ?></td>
+            <td>
+                <a href="update.php?id=<?= $row["id"] ?>">Edit</a> |
+                <a href="hapus.php?id=<?= $row["id"] ?>" onclick="return confirm('yakin?');">Hapus</a>
+            </td>
+            <td><img src="../img/<?= $row["gambar"] ?>" height="50px" width="50px"></td>
+            <td><?= $row["nim"] ?></td>
+            <td><?= $row["nama"] ?></td>
+            <td><?= $row["email"] ?></td>
+            <td><?= $row["jurusan"] ?></td>
+        </tr>
+        <?php $i++ ?>
+        <?php endforeach; ?>
+    </table>
+
+
+</body>
+</html>
