e427ba383e9a5e1c5f8c9e9df2c313d7fd585fc4
2503010055-Fahri <2503010055@unper.ac.id>
2026-07-01 20:57:11 +0700

    Pagination PHP dasar

---
+++ Pagination/tambah.php
@@ -0,0 +1,74 @@
+<?php
+session_start();
+
+if( !isset($_SESSION["login"]) ){
+    header("Location: login.php");
+    exit;
+}
+
+require("function.php");
+// konek database
+$conn = mysqli_connect("localhost", "root", "", "phpdasar");
+
+// cek apakah tobol submit sudah di tekan?
+if( isset($_POST["submit"]) ){
+
+    // cek apakah data berhasil ditambah atau tidak
+    if (tambah($_POST) > 0){
+        echo "<script>
+                alert('data berhasil ditambahkan')
+                document.location.href = 'index.php'
+            </script>";
+    } else {
+        echo "<script>
+                alert('data gagal ditambahkan')
+                documnet.location.href = 'index.php'
+            </script>";;
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Data Mahasiswa</title>
+</head>
+<body>
+    
+    <h1>Tambah Data Mahasiswa</h1>
+
+    <form action="" method="post" enctype="multipart/form-data">
+        <ul>
+            <li>
+                <label for="nama">Nama : </label>
+                <input type="text" name="nama" id="nama" required>
+            </li>
+            <li>
+                <label for="nim">NIM : </label>
+                <input type="text" name="nim" id="nim" required>
+            </li>
+            <li>
+                <label for="email">Email : </label>
+                <input type="email" name="email" id="email" required>
+            </li>
+            <li>
+                <label for="jurusan">Jurusan : </label>
+                <input type="text" name="jurusan" id="jurusan" required>
+            </li>
+            <li>
+                <label for="gambar">Gambar : </label>
+                <input type="file" name="gambar" id="gambar" required>
+            </li>
+            <li>
+                <button type="submit" name="submit">Simpan</button>
+            </li>
+            <li>
+                <a href="index.php">Kembali ke daftar mahasiswa</a>
+            </li>
+        </ul>
+    </form>
+
+</body>
+</html>
---
+++ Pagination/update.php
@@ -0,0 +1,82 @@
+<?php
+session_start();
+
+if( !isset($_SESSION["login"]) ){
+    header("Location: login.php");
+    exit;
+}
+
+require("function.php");
+// konek database
+$conn = mysqli_connect("localhost", "root", "", "phpdasar");
+
+// ambil data dari URL
+$id = $_GET["id"];
+// query data mahasiswa berdasarkan id-nya
+$mhs = query("SELECT * FROM mahasiswa WHERE id = $id")[0];
+
+// cek apakah tobol submit sudah di tekan?
+if( isset($_POST["submit"]) ){
+
+    // cek apakah data berhasil diupdate
+    if (update($_POST) > 0){
+        echo "<script>
+                alert('data berhasil diupdate')
+                document.location.href = 'index.php'
+            </script>";
+    } else {
+        echo "<script>
+                alert('data gagal diupdate')
+                documnet.location.href = 'index.php'
+            </script>";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Data Mahasiswa</title>
+</head>
+<body>
+    
+    <h1>Update Data Mahasiswa</h1>
+
+    <form action="" method="post" enctype="multipart/form-data">
+        <input type="hidden" name="id" value="<?= $mhs["id"] ?>">
+        <input type="hidden" name="gambarLama" value="<?= $mhs["gambar"] ?>">
+        <ul>
+            <li>
+                <label for="nama">Nama : </label>
+                <input type="text" name="nama" id="nama" required value="<?= $mhs["nama"] ?>">
+            </li>
+            <li>
+                <label for="nim">NIM : </label>
+                <input type="text" name="nim" id="nim" required value="<?= $mhs["nim"] ?>">
+            </li>
+            <li>
+                <label for="email">Email : </label>
+                <input type="email" name="email" id="email" required value="<?= $mhs["email"] ?>">
+            </li>
+            <li>
+                <label for="jurusan">Jurusan : </label>
+                <input type="text" name="jurusan" id="jurusan" required value="<?= $mhs["jurusan"] ?>">
+            </li>
+            <li>
+                <label for="gambar">Gambar : </label>
+                <img src="../img/<?= $mhs["gambar"]; ?>" width="100">
+                <input type="file" name="gambar" id="gambar">
+            </li>
+            <li>
+                <button type="submit" name="submit">Simpan</button>
+            </li>
+            <li>
+                <a href="index.php">Kembali ke daftar mahasiswa</a>
+            </li>
+        </ul>
+    </form>
+
+</body>
+</html>
